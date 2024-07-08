5f2f1df9f0698679c2682d52ec38a776b09159aa
Matheussmaced <mateusbyakuya3@gmail.com>
2024-07-08 12:02:22 -0300

    feat: creating games class

---
+++ src/main/java/com/todogames/todogames/entity/Games.java
@@ -0,0 +1,65 @@
+package com.todogames.todogames.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_games")
+public class Games {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private UUID id;
+
+  @Column(name = "gameName")
+  private String name;
+
+  @Column(name = "description")
+  private String description;
+
+  @Column(name = "completed")
+  private boolean completed;
+
+  public Games() {
+  }
+
+  public Games(UUID id, String name, String description, boolean completed) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.completed = completed;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public boolean isCompleted() {
+    return completed;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public void setCompleted(boolean completed) {
+    this.completed = completed;
+  }
+
+}
