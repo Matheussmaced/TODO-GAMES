ce43cb14b9aae78940a40a9b4078d2495c445c39
Matheussmaced <mateusbyakuya3@gmail.com>
2024-07-08 11:54:56 -0300

    feat: creating user class

---
+++ src/main/java/com/todogames/todogames/entity/User.java
@@ -0,0 +1,81 @@
+package com.todogames.todogames.entity;
+
+import java.util.UUID;
+import java.time.Instant;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID userId;
+
+  @Column(name = "userName")
+  private String name;
+
+  @Column(name = "password")
+  private String password;
+
+  @CreationTimestamp
+  private Instant creationTimestamp;
+
+  @UpdateTimestamp
+  private Instant updateTimeInstant;
+
+  public User() {
+  }
+
+  public User(UUID userId, String name, String password, Instant creationTimestamp, Instant updateTimeInstant) {
+    this.userId = userId;
+    this.name = name;
+    this.password = password;
+    this.creationTimestamp = creationTimestamp;
+    this.updateTimeInstant = updateTimeInstant;
+  }
+
+  public UUID getUserId() {
+    return userId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public Instant getCreationTimestamp() {
+    return creationTimestamp;
+  }
+
+  public Instant getUpdateTimeInstant() {
+    return updateTimeInstant;
+  }
+
+  public void setUserId(UUID userId) {
+    this.userId = userId;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public void setCreationTimestamp(Instant creationTimestamp) {
+    this.creationTimestamp = creationTimestamp;
+  }
+
+  public void setUpdateTimeInstant(Instant updateTimeInstant) {
+    this.updateTimeInstant = updateTimeInstant;
+  }
+
+}
